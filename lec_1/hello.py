9a3ac3ada756dca6b40e9cf91fa25d50ac548474
yolo22nd <omtank22@gmail.com>
2024-06-19 22:24:36 +0530

    first commit

---
+++ lec_1/hello.py
@@ -0,0 +1,215 @@
+# print("hello world")
+# print('a')
+# # ''
+# # ""
+
+
+# defining variables
+# a = 4
+# b = 'c'
+# x = 5.45
+
+# # printing stuff
+# print("hwgw")
+# # no semicolons
+
+# temp = True
+# notTemp= False
+
+# print(temp)
+
+# type casting
+# new = "12"
+# new3 = 12.3435
+
+# print(float(new3))
+
+# temp= new+  '   1'
+# print(temp)
+
+
+# tempp= int(new3)
+# print(tempp)
+# lists
+numbers= [1,2,3,'apple','mango',True,24.65]
+
+# print(numbers[-2])
+
+# dictionary
+# key value pairs
+
+# {}
+# flist = ['apple','mango','banana']
+
+# fruits= {
+#     'b':'banana',
+#     'm':'mango',
+#     'a': 'apple'
+# }
+
+# print(fruits['a'])
+
+
+# nested list
+abcd = ['apple','mango',[1,2,3],'banana']
+
+# print(abcd[1][0:5])
+
+# nested dictionary
+
+fruits= {
+    'b':'banana',
+    'm':'mango',
+    'a': 'apple',
+    'dict': {'1':'11',1:22,'3':'33',4:1351511},
+    'list':abcd,
+    1:'not1'
+}
+
+# print(fruits['list'][2][2])
+# print(fruits['dict'][1])
+
+
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : abcd
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+
+
+# # # print(myfamily['child3']["name"])
+
+# print(myfamily['child1']['year'][0])
+
+
+
+# conditional statements
+
+# a=3
+# print('b is 5')
+
+# if a == 5:
+#     print('a is 5')
+    
+# elif a == 3:
+#     print('a is 4')
+    
+# else:
+#     print('a is not 5')
+    
+  
+# print('b is 5')
+  
+  
+# loops
+j=10
+abcd = ['apple','mango',[1,2,3],'banana']
+# print(abcd)
+# for i in range(start, stop+1, step):
+# var=0121
+
+var=1
+# p=1
+
+# p = 2
+# for i in range(1,4):
+#     q = p
+#     for j in range (1,q):
+#         p = p + 1
+#     print(p-1)
+
+
+# var=1
+# for i in range(0, 4) :
+#     print(var) 
+#     var=var+var
+
+# for i in abcd:
+# #     print(i)
+
+# while condition:
+#     print('hi')
+#     break
+
+    
+
+
+1,2,4,8
+# print(2 in [1,[2],3,4])
+
+
+
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : abcd
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+
+
+
+# fruits= {
+#     'b':'banana',
+#     'm':'mango',
+#     'a': 'apple',
+#     'dict': {'1':'11',1:22,3:'33',4:1351511},
+#     'list':abcd,
+#     1:'not1'
+# }
+
+# for var in fruits:
+#     print(fruits[var][3])
+    # print(var)
+    # print(i)
+    # print('hello')
+    # j=j+1
+    # print(j)
+    # print(abcd[i][-i])
+    
+# print(abcd[-4])
+
+# aa3a
+
+
+
+
+q1# take an input from user is should be a number and find out how many digit it has
+
+
+q2# take an input from the user the factorial of a number if the user enters a string print not a valid input
+
+#.isalpha()
+
+q3# create a program
+
+Computers ={
+  "Laptop1" : {"brand" : "DELL","OS" : "Windows"},
+  "Laptop2" : {"brand" : "HP", "OS" : "Linux"},
+  "Desktop" : {"brand" : "Apple","OS" : "Mac-OS"}
+}
+# from this above data create a list brand,OS
+#print(brand)
+#['brand','hp','apple']
+#print(os)
+#['Windows','Linux','MAc-os']
+
+
+
+https://leetcode.com/problems/two-sum/description/
